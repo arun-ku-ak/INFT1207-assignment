366fef89f3723809a375fb6970f111d5264703d4
01Arun-devs <arunkum1904@gmail.com>
2025-02-20 20:59:46 -0500

    Update test_suite.py
    commit

--- Lab3_Group4/test_suite.py
+++ Lab3_Group4/test_suite.py
@@ -17,10 +17,15 @@ def run_tests(choice):
         suite.addTest(TestShapes('test_circle_area_invalid'))
     elif choice == 't':
         suite.addTest(TestShapes('test_trapezium_area_valid'))
+        suite.addTest(TestShapes('test_trapezium_area_invalid'))
     elif choice == 'e':
         suite.addTest(TestShapes('test_ellipse_area_valid'))
+        suite.addTest(TestShapes('test_ellipse_area_invalid'))
+
     elif choice == 'r':
         suite.addTest(TestShapes('test_rhombus_area_valid'))
+        suite.addTest(TestShapes('test_rhombus_area_invalid'))
+
     else:
         print("Invalid choice. Exiting.")
         return
@@ -30,6 +35,11 @@ def run_tests(choice):
 
 
 if __name__ == "__main__":
-    print("Enter a shape to test ('c' for Circle, 't' for Trapezium, 'e' for Ellipse, 'r' for Rhombus):")
+    print("Enter a shape to test:\n"
+          "'c' for Circle\n"
+          "'t' for Trapezium\n"
+          "'e' for Ellipse\n"
+          "'r' for Rhombus\n"
+          "What would you like to choose: ")
     choice = input().strip().lower()
     run_tests(choice)
